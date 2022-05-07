4bfa16a5aaebe350e5fcdaa7a87dbedb7fefb6c5
CeViMu <vce02060@innovaccion.mx>
2022-05-07 18:47:03 -0500

    Corrigiendo refactorizacion de filtrar por missión y su prueba

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,16 +2,10 @@ const ExplorerService = require('./../../lib/services/ExplorerService')
 const Reader = require("./../../lib/utlis/Reader");
 
 describe("Unit test", () =>{
-    test("Test para filtrar a los explorers que pertenecen a una mission", () =>{
+    test("1) Test para filtrar a los explorers que pertenecen a una mission", () =>{
         const explorers = Reader.readJsonFile("explorers.json");
         const explorersInNode = ExplorerService.filterByMission(explorers,"node")
         
-        expect(explorersInNode[2]).toMatchObject({
-            "name": "Woopa3",
-            "githubUsername": "ajolonauta3",
-            "score": 3,
-            "mission": "node",
-            "stacks": ["elixir", "groovy", "reasonML"]
-          })
+        expect(explorersInNode).toStrictEqual(explorers.filter((explorer) => explorer.mission == "node"))
     }) 
 }) 
