d97bb8ae40beeea6e37733cdc2eaf424ffaf8159
CeViMu <vce02060@innovaccion.mx>
2022-05-07 17:31:35 -0500

    Agregando prueba para Reader

---
+++ test/utlis/Reader.test.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const Reader = require('../../lib/utlis/Reader')
+
+describe("Unit test", () =>{
+    test("Test for class Reader", () =>{
+        const reader = Reader.readJsonFile('explorers.json')
+        const data = fs.readFileSync('explorers.json')
+        expect(reader).toBe(JSON.parse(data))
+    }) 
+}) 
