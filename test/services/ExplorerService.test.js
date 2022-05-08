2b2843805a25e4783e9bc40b81166f5f0549b804
CeViMu <vce02060@innovaccion.mx>
2022-05-08 14:04:19 -0500

    Agregando mÃetodo y prueba para getExplorersUsernamesByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -12,4 +12,13 @@ describe("Unit test", () =>{
         const explorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersByMission).toBe(2);
     }) 
+
+    test("3) Test para obtener los nombres de usuarios en una mission", () =>{
+        const explorers = [ 
+            {mission: "java", githubUsername: "ajolonauta1"}, 
+            {mission: "node", githubUsername: "ajolonauta2"},
+            {mission: "java", githubUsername: "ajolonauta3"}]
+        const usernameExplorersInJavaMission = ExplorerService.getExplorersUsernamesByMission(explorers, "java")
+        expect(usernameExplorersInJavaMission).toEqual(["ajolonauta1", "ajolonauta3"])
+    })
 }) 
