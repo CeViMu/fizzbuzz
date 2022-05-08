3059d7ccee4738a23d608b5f47312f1f2456ded4
CeViMu <vce02060@innovaccion.mx>
2022-05-07 19:08:42 -0500

    Modificando prueba para filtrar explorers de una misión

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,11 +1,9 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
-const Reader = require("./../../lib/utlis/Reader");
 
 describe("Unit test", () =>{
     test("1) Test para filtrar a los explorers que pertenecen a una mission", () =>{
-        const explorers = Reader.readJsonFile("explorers.json");
-        const explorersInNode = ExplorerService.filterByMission(explorers,"node")
-        
-        expect(explorersInNode).toStrictEqual(explorers.filter((explorer) => explorer.mission == "node"))
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
     }) 
 }) 
