a981712364f59d76dc4a0754d0b605af27a6b389
CeViMu <vce02060@innovaccion.mx>
2022-05-09 18:50:14 -0500

    Modificando prueba

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,7 +2,6 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Unit test", () =>{
     test("1) Test para obtener la lista de explorers de la mision node", () => {
-        //const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerController.getExplorersByMission("node");
         expect(explorersInNode.length).toBe(10);
     }) 
