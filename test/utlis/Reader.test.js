1fcc4891210612cbb1e65d99b70ba9ce7981ef66
CeViMu <vce02060@innovaccion.mx>
2022-05-09 10:04:50 -0500

    Corrigiendo prueba

--- test/utlis/Reader.test.js
+++ test/utlis/Reader.test.js
@@ -5,6 +5,6 @@ describe("Unit test", () =>{
     test("Test for class Reader", () =>{
         const reader = Reader.readJsonFile('explorers.json')
         const data = fs.readFileSync('explorers.json')
-        expect(reader).toBe(JSON.parse(data))
+        expect(reader).toStrictEqual(JSON.parse(data))
     }) 
 }) 
