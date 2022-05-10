5b2a11e167efb9a12e18038e6d2b6228c41b66bb
CeViMu <vce02060@innovaccion.mx>
2022-05-09 19:05:50 -0500

    Agregando metodo y prueba para obtener los nombres de usuarios por mision en en controller

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -5,4 +5,10 @@ describe("Unit test", () =>{
         const explorersInNode = ExplorerController.getExplorersByMission("node");
         expect(explorersInNode.length).toBe(10);
     }) 
+
+    test("2) Test para obtener la lista nombres de usuarios de la mision node", () => {
+        const usernames = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(usernames).toEqual(["ajolonauta1", "ajolonauta2","ajolonauta3", "ajolonauta4", "ajolonauta5",
+        "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]);
+    }) 
 })
